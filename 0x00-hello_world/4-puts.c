d6b83d86292f0a7d03a54255f8f0caa6a4a0f8a1
b-kemmy <beatricegesisa@gmail.com>
2023-06-15 15:22:38 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,21 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ 
+ *main-Entry point
+
+ *
+ 
+ *Return: Always 0 (Success)
+
+ */
+
+int main(void)
+
+{
+
+   puts("\"Programming is like building amultilingual puzzle");
+   return (0);
+   
+}	
