aace1b7f83567061771da158ce344534e2729afd
b-kemmy <beatricegesisa@gmail.com>
2023-06-17 11:31:40 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,17 +1,11 @@
-/**
- *
- *File: 0-positive_or_negative.c 
- *Auth: b-kemmy
- */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-/**
- *main - Prints a random number and determines whether it is positive, negative or zero
- *
- *Return: Always 0 (Success)
+ /**
+  *main - Prints a random number and determines whether it is positive, negative or zero
+  *
+  *Return: Always 0 (Success)
  */
 
 int main(void)
