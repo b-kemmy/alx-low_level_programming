a86c50e17005eb09543a2c6453cc695d4da520ff
b-kemmy <beatricegesisa@gmail.com>
2023-06-19 07:24:28 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out the letters of the alphabet a-z
+ *
+ * Returns - Always 0 (Succes)
+ *
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+	{
+	
+	putchar(a);
+	
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}	
