7ed7c118df173c74ba07f42637075212228dbb67
b-kemmy <beatricegesisa@gmail.com>
2023-06-25 10:44:42 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num <= 9; num++)
 	{
 		putchar((num % 10) + '0');
@@ -23,5 +23,5 @@ int main(void)
 
 	putchar ('\n');
 
-	return(0);
+	return (0);
 }
