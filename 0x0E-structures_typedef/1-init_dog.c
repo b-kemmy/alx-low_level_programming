b7150f9ce27aa4b98e11488cf04e352fbc283af6
b-kemmy <beatricegesisa@gmail.com>
2023-07-17 05:50:11 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -11,13 +11,13 @@
  * Return: Always 0 (Success)
  */
 
-void init_dog(struct dog *d, vhar *name, float age, char *owner)
+void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
 		d->name = name;
 		d->age = age;
-		d->ownwer = owner;
+		d->owner = owner;
 	}
 
 }
