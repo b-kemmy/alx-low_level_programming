2830a6d115aea993cd76ec366778e4c3135f7c9f
b-kemmy <beatricegesisa@gmail.com>
2023-07-17 07:07:58 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -27,5 +27,4 @@ void init_dog(struct dog *d, char *name, float age, char *ownwer);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
